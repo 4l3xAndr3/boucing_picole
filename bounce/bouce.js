bb818242046ceb8dbaf9a46c42a93a213e2be70a
l3g0craft <96246326+l3g0craft@users.noreply.github.com>
2023-03-15 16:39:35 +0100

    bounce v2
    rebond avec 5 tete

--- bounce/bouce.js
+++ bounce/bouce.js
@@ -1,13 +1,33 @@
-const image1 = document.getElementById("myImage1");
-const image2 = document.getElementById("myImage2");
-let x1 = 0,
-    y1 = 0,
+const image1 = document.getElementById("dodo");
+const image2 = document.getElementById("alex");
+const image3 = document.getElementById("augustin");
+const image4 = document.getElementById("nathan");
+const image5 = document.getElementById("simon");
+let x1 = Math.floor(Math.random() * (800 - 1)),
+    y1 = Math.floor(Math.random() * (500 - 1)),
     dx1 = 5,
     dy1 = 3,
-    x2 = 300,
-    y2 = 100,
+
+    x2 = Math.floor(Math.random() * (900 - 1)),
+    y2 = Math.floor(Math.random() * (500 - 1)),
     dx2 = -4,
     dy2 = 2,
+
+    x3 = Math.floor(Math.random() * (900 - 1)),
+    y3 = Math.floor(Math.random() * (500 - 1)),
+    dx3 = 5,
+    dy3 = 3,
+
+    x4 = Math.floor(Math.random() * (900 - 1)),
+    y4 = Math.floor(Math.random() * (500 - 1)),
+    dx4 = 5,
+    dy4 = 3,
+
+    x5 = Math.floor(Math.random() * (900 - 1)),
+    y5 = Math.floor(Math.random() * (500 - 1)),
+    dx5 = 5,
+    dy5 = 3,
+
     w = window.innerWidth,
     h = window.innerHeight;
 
@@ -16,6 +36,12 @@ function animate() {
     y1 += dy1;
     x2 += dx2;
     y2 += dy2;
+    x3 += dx3;
+    y3 += dy3;
+    x4 += dx4;
+    y4 += dy4;
+    x5 += dx5;
+    y5 += dy5;
 
     // rebondissement pour l'image 1
     if (x1 < 0 || x1 > w - image1.width) {
@@ -37,11 +63,41 @@ function animate() {
         dy2 += Math.random() * 2 - 1;
     }
 
+    // rebondissement pour l'image 3
+    if (x3 < 0 || x3 > w - image3.width) {
+        dx3 = -dx3;
+    }
+    if (y3 < 0 || y3 > h - image3.height) {
+        dy3 = -dy3;
+    }
+
+    // rebondissement pour l'image 4
+    if (x4 < 0 || x4 > w - image4.width) {
+        dx4 = -dx4;
+    }
+    if (y4 < 0 || y4 > h - image4.height) {
+        dy4 = -dy4;
+    }
+
+    // rebondissement pour l'image 5
+    if (x5 < 0 || x5 > w - image5.width) {
+        dx5 = -dx5;
+    }
+    if (y5 < 0 || y5 > h - image5.height) {
+        dy5 = -dy5;
+    }
+
     // déplacer les images
     image1.style.left = x1 + "px";
     image1.style.top = y1 + "px";
     image2.style.left = x2 + "px";
     image2.style.top = y2 + "px";
+    image3.style.left = x3 + "px";
+    image3.style.top = y3 + "px";
+    image4.style.left = x4 + "px";
+    image4.style.top = y4 + "px";
+    image5.style.left = x5 + "px";
+    image5.style.top = y5 + "px";
 
     requestAnimationFrame(animate);
 }
