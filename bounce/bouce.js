615be30c8790dda47ae8d40e9645825fd461bad2
l3g0craft <96246326+l3g0craft@users.noreply.github.com>
2023-03-15 14:50:00 +0100

    Update bouce.js
    t

--- bounce/bouce.js
+++ bounce/bouce.js
@@ -1,76 +1,49 @@
-//img1
-var img1 = document.getElementById("myImage1");
-var x1 = 0;
-var y1 = 0;
-var dx1 = 4;
-var dy1 = 4;
-var width1 = window.innerWidth - img1.width;
-var height1 = window.innerHeight - img1.height;
-//img2
-var img2 = document.getElementById("myImage2");
-var x2 = 100;
-var y2 = 100;
-var dx2 = 3;
-var dy2 = 3;
-var width2 = window.innerWidth - img2.width;
-var height2 = window.innerHeight - img2.height;
-/*img3
-var img3 = document.getElementById("myImage3");
-var x3 = 100;
-var y3 = 100;
-var dx3 = 3;
-var dy3 = 3;
-var width3 = window.innerWidth - img3.width;
-var height3 = window.innerHeight - img3.height;
-*/
+const image1 = document.getElementById("myImage1");
+const image2 = document.getElementById("myImage2");
+let x1 = 0,
+    y1 = 0,
+    dx1 = 5,
+    dy1 = 3,
+    x2 = 300,
+    y2 = 100,
+    dx2 = -4,
+    dy2 = 2,
+    w = window.innerWidth,
+    h = window.innerHeight;
+
 function animate() {
-x1 += dx1;
-y1 += dy1;
-x2 += dx2;
-y2 += dy2;
+    x1 += dx1;
+    y1 += dy1;
+    x2 += dx2;
+    y2 += dy2;
 
-if (x1 > width1 || x1 < 0) {
-    dx1 = -dx1;
-    dx1 += Math.random() * 10 - 5;
-}
-if (y1 > height1 || y1 < 0) {
-    dy1 = -dy1;
-    dy1 += Math.random() * 10 - 5;
-}
+    // rebondissement pour l'image 1
+    if (x1 < 0 || x1 > w - image1.width) {
+        dx1 = -dx1;
+    }
+    if (y1 < 0 || y1 > h - image1.height) {
+        dy1 = -dy1;
+    }
 
-if (x2 > width2 || x2 < 0) {
-    dx2 = -dx2;
-    dx2 += Math.random() * 10 - 5;
-}
-if (y2 > height2 || y2 < 0) {
-    dy2 = -dy2;
-    dy2 += Math.random() * 10 - 5;
-}
+    // rebondissement pour l'image 2
+    if (x2 < 0 || x2 > w - image2.width) {
+        dx2 = -dx2;
+        dx2 += Math.random() * 2 - 1;
+        dy2 += Math.random() * 2 - 1;
+    }
+    if (y2 < 0 || y2 > h - image2.height) {
+        dy2 = -dy2;
+        dx2 += Math.random() * 2 - 1;
+        dy2 += Math.random() * 2 - 1;
+    }
 
-/* calculate distance between the two images
-var distance = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
-if (distance < img1.width) {
-    // reverse direction of each image
-    dx1 = -dx1;
-    dy1 = -dy1;
-    dx2 = -dx2;
-    dy2 = -dy2;
-    
-    // set random directions for each image
-    dx1 += Math.random() * 10 - 5;
-    dy1 += Math.random() * 10 - 5;
-    dx2 += Math.random() * 10 - 5;
-    dy2 += Math.random() * 10 - 5;
-}
-*/
-// update image positions
-img1.style.left = x1 + "px";
-img1.style.top = y1 + "px";
-img2.style.left = x2 + "px";
-img2.style.top = y2 + "px";
+    // déplacer les images
+    image1.style.left = x1 + "px";
+    image1.style.top = y1 + "px";
+    image2.style.left = x2 + "px";
+    image2.style.top = y2 + "px";
 
-requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
 }
 
 animate();
-
